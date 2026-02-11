f966fc772fcca69d3cf9513d4c1f28542e0302a0
seyoung <tpdudtpdud128@gmail.com>
2026-02-11 16:13:29 +0900

    Bean이 여러 개일 때

---
+++ src/test/java/com/sparta/springauth/BeanTest.java
@@ -0,0 +1,26 @@
+package com.sparta.springauth;
+
+import com.sparta.springauth.food.Food;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeanTest {
+
+    @Autowired
+    Food food;
+
+    @Autowired
+    @Qualifier("pizza")
+     Food food1;
+
+    @Test
+    @DisplayName("우선순위 Priamary Bean 선택")
+    void test1(){
+        food.eat();
+    }
+
+}
